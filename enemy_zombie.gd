9b600eef06b5459b0c8107a0bcbc85a2e50b4e68
HackrDonut <luke.a.pruitt@gmail.com>
2023-12-06 11:14:55 -0600

    Random Note Generation
    The basic enemy can now generate a random note!

--- enemy_zombie.gd
+++ enemy_zombie.gd
@@ -2,10 +2,13 @@ extends Sprite2D
 
 var rng = RandomNumberGenerator.new()
 var note = ""
+@onready var nsprite = get_node("Random_Note")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var rand = rng.randf_range(0, 7)
+	var rand = rng.randf_range(0, 10)
+	rand = int(round(rand))
 	
 	if rand == 0:
 		note = "C1"
@@ -23,6 +26,35 @@ func _ready():
 		note = "B1"
 	elif rand == 7:
 		note = "C2"
+	elif rand == 8:
+		note = "D2"
+	elif rand == 9:
+		note = "E2"
+	elif rand == 10:
+		note = "F2"
+	
+	if note == "C1":
+		nsprite.frame = 8
+	elif note == "D1":
+		nsprite.frame = 11
+	elif note == "E1":
+		nsprite.frame = 14
+	elif note == "F1":
+		nsprite.frame = 15
+	elif note == "G1":
+		nsprite.frame = 18
+	elif note == "A1":
+		nsprite.frame = 21
+	elif note == "B1":
+		nsprite.frame = 24
+	elif note == "C2":
+		nsprite.frame = 25
+	elif note == "D2":
+		nsprite.frame = 28
+	elif note == "E2":
+		nsprite.frame = 31
+	elif note == "F2":
+		nsprite.frame = 32
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
